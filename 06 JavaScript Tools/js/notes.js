93db88be02d8275fad4e57d279107edc9508bb65
Marko Korhonen <reekymarko@reekynet.com>
2017-11-08 13:10:29 +0200

    Finished up 4, 5 and 6

--- 06 JavaScript Tools/js/notes.js
+++ 06 JavaScript Tools/js/notes.js
@@ -1,33 +1,46 @@
-
+'use strict';
 var notes = new Array();
+loadList();
 
 function addItem() {
-	textbox = document.getElementById('item');
-	var itemText = textbox.value;
-	textbox.value = '';
-	textbox.focus();
-	var newItem = {title: itemText, quantity: 1};
-	notes.push(newItem);
-	displayList();
+    var textbox = document.getElementById('item');
+    var itemText = textbox.value;
+    textbox.value = '';
+    textbox.focus();
+    var newItem = {title: itemText, quantity: 1};
+    notes.push(newItem);
+    displayList();
+    saveList();
 }
 
 function displayList() {
-	var table = document.getElementById('list');
-	table.innerHTML = '';
-	for (var i = 0; i<notes.length; i++) {
-		var node = undefined;
-		var note = notes[i];
-		var node = document.createElement('tr');
-		var html = '<td>'+note.title+'</td><td>'+note.quantity+'</td><td><a href="#" onClick="deleteIndex('+i+')">delete</td>';
-	    node.innerHTML = html;
-		table.appendChild(node);
-	}
+    var table = document.getElementById('list');
+    table.innerHTML = '';
+    for (var i = 0; i<notes.length; i++) {
+        var node = undefined;
+        var note = notes[i];
+        node = document.createElement('tr');
+        var html = '<td>'+note.title+'</td><td>'+note.quantity+'</td><td><a href="#" onClick="deleteIndex('+i+')">delete</td>';
+        node.innerHTML = html;
+        table.appendChild(node);
+    }
 }
 
 function deleteIndex(i) {
-	notes.splice(i, 1);
-	displayList();
+    notes.splice(i, 1);
+    displayList();
+}
+function saveList() {
+    localStorage.notes = JSON.stringify(notes);
 }
 
-button = document.getElementById('add');
+function loadList() {
+    console.log('loadList');
+    if (localStorage.notes) {
+        notes = JSON.parse(localStorage.notes);
+        displayList();
+    }
+}
+
+var button = document.getElementById('add');
 button.onclick = addItem;
