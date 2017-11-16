a5f5a924081a0ba8bc4eb9c58a53c12e0379ec75
Marko Korhonen <reekymarko@reekynet.com>
2017-11-16 16:46:57 +0200

    Did part of 9

--- 09 Single-Page Applications/js/building_lists.js
+++ 09 Single-Page Applications/js/building_lists.js
@@ -6,12 +6,67 @@ var data = JSON.parse(request.responseText);
 console.log(data);
 
 var books = data.books;
+var currentBook;
 
-var list = document.createElement('ol');
+var table = document.createElement('table');
+
+var th = document.createElement('th');
+th.setAttribute("style", "border-style: solid; border-width: 1px; border-collapse: collapse;");
+th.innerHTML = 'Title';
+table.appendChild(th);
+
+var th = document.createElement('th');
+th.setAttribute("style", "border-style: solid; border-width: 1px; border-collapse: collapse;");
+th.innerHTML = 'Year';
+table.appendChild(th);
+
+var th = document.createElement('th');
+th.setAttribute("style", "border-style: solid; border-width: 1px; border-collapse: collapse;");
+th.innerHTML = 'ISBN';
+table.appendChild(th);
+
+var th = document.createElement('th');
+th.setAttribute("style", "border-style: solid; border-width: 1px; border-collapse: collapse;");
+th.innerHTML = 'Authors';
+table.appendChild(th);
+
+table.setAttribute("style", "border-style: solid; border-width: 1px; border-collapse: collapse;");
+
 for (var i=0; i < books.length; i++) {
-	console.log(books[i].title);
-	var item = document.createElement('li');
-	item.innerHTML = books[i].title;
-	list.appendChild(item);
+    currentBook = i;
+    console.log(books[i].title);
+
+    var tr = document.createElement('tr');
+    tr.setAttribute("style", "border-style: solid; border-width: 1px; border-collapse: collapse;");
+
+    var td = document.createElement('td');
+    td.innerHTML = books[i].title;
+    td.setAttribute("style", "border-style: solid; border-width: 1px; border-collapse: collapse;");
+    tr.appendChild(td);   
+
+    var td = document.createElement('td');
+    td.innerHTML = books[i].year;
+    td.setAttribute("style", "border-style: solid; border-width: 1px; border-collapse: collapse;");
+    tr.appendChild(td);
+
+    var td = document.createElement('td');
+    td.innerHTML = books[i].isbn;
+    td.setAttribute("style", "border-style: solid; border-width: 1px; border-collapse: collapse;");
+    tr.appendChild(td);
+
+    var td = document.createElement('td');
+    td.innerHTML = books[i].authors;
+    td.setAttribute("style", "border-style: solid; border-width: 1px; border-collapse: collapse;");
+    tr.appendChild(td);
+
+    table.appendChild(tr);
 }
-document.body.appendChild(list);
+
+table.addEventListener('click', function(e){
+    if (e.target.nodeName.toUpperCase() !== "TD") return;
+    var td = e.target
+    document.getElementById("displaySelected").innerHTML = td.innerText;
+});
+
+document.body.appendChild(table);
+document.getElementById("displaySelected").innerHTML = "Version Control with Git";
